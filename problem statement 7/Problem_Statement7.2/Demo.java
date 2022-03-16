53ba6250574b31a1e9887330920a3abf8bc3076e
BULIGONDLA APARNA <97727311+aparna414@users.noreply.github.com>
2022-03-16 05:44:51 -0700

    Add files via upload

---
+++ problem statement 7/Problem_Statement7.2/Demo.java
@@ -0,0 +1,9 @@
+package Problem_Statement7.2;
+
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
